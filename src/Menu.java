38c2b7fd4f6f1643f95b4e7655968ae77f8bfe03
syedzayed <syed.omar@ucalgary.ca>
2024-02-22 13:00:37 -0700

    Menu initialization also complete. More menu options are needed

---
+++ src/Menu.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Menu {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static final ArrayList<String> options = new ArrayList<>();
+    static {
+        options.add("Exit");
+        //Add more menu options
+    }
+    //The method below is the intro message that shows up above the menu.
+    private static String optMessage = """
+            Store and access details of the options and actions of the members in the party and also details about foe.
+            \tMenu Options
+            """;
+    static{
+            StringBuilder sb = new StringBuilder();
+            sb.append(optMessage);
+            for (int i = 0; i < options.size(); i++) {
+                sb.append(String.format("\t%d. %s\n", i, options.get(i)));
+            }
+            optMessage = sb.toString();
+    }
+
+
+    public static void menuLoop(){
+        System.out.println(optMessage);
+    }
+}
