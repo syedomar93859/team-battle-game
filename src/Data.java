d23b39a50066e0bc8f88dadb35dde1ffd15c18cc
syedzayed <syed.omar@ucalgary.ca>
2024-02-24 23:05:53 -0700

    Some ideas of how to go about working on certain methods to accomplish certain options

--- src/Data.java
+++ src/Data.java
@@ -54,7 +54,73 @@ run
 assist - raise health by 1500 hp for one party member
 */
 public class Data {
-    HashMap<String, Integer> startingHealth = new Hashmap<String, Integer>();
+    public static HashMap<String,Integer> beginningHealth(){
+        HashMap<String, Integer> startingHealth = new HashMap<String, Integer>();
+        startingHealth.put("Potioneer", 2000);
+        startingHealth.put("Swordsman", 2500);
+        startingHealth.put("Shield User", 5000);
+        startingHealth.put("Slime Boss", 10000);
+        return startingHealth;
+    }
+
+    public static int actionCheck(String member,int action, int attackAction, String affectedMember) {
+        //attackDetails is a method that I have not created yet that will calculate the whatever the attack is of the member
+        //assistDetermining is a method that determines if an assist is successful as they have a chance to fail
+        //a is a method that
+        if (member == "Potioneer") {
+            //I don't think there should be an if statement checking to see if the option chosen was Exit, as Menu.java
+            //alone can deal with it
+            if (action == 2) {
+                int damage = Data.attackDetails(String member, int attackPotioneerAction);
+                return damage;
+            }
+            //I also think Menu.java can deal with Run.
+            else if (action == 4) {
+                boolean assistSuccess = Data.assistDetermining(String member);
+                if (assistSuccess == true) {
+                    return -1;//You should probably come up with something more complicated than -1. Since all the calculating needs to happen in Data.java, there
+                    //could be another method that calulates  the improved health of all the members.
+                } else if (assistSuccess == false) {
+                    return -2;
+                }
+                //I was thinking that Menu.java can use the returned negative number to determine is the assist worked or not.
+            }
+        } else if (member == "Swordsman") {
+            if (action == 2) {
+                int damage = Data.attackDetails(String member, int attackSwordsmanAction);
+                return damage;
+            } else if (action == 4) {
+                boolean assistSuccess = Data.assistDetermining(String member);
+                if (assistSuccess == true) {
+                    return -1;// I think Menu.java should take this -1 and let a loop go on for 3 rounds which would call a different method separate from actionCheck
+                    //in each round to calculate the damage dealt by new attacks.
+                } else if (assistSuccess == false) {
+                    return -2;
+                }
+            }
+        } else if (member == "Shield User") {
+            if (action == 2) {
+                int damage = Data.attackDetails(String member, int attackShieldUserAction);
+                return damage;
+            } else if (action == 4) {
+                boolean assistSuccess = Data.assistDetermining(String member);
+                if (assistSuccess == true) {
+                    int memberImprovedHealth = Data.assistNewHealth(affectedMember);
+                    //memberImprovedHealth should instead store the improved health of all party members which is not possible with the current actionCheck as it can only return
+                    //int and not hashmaps which would be more suitable.
+                    return memberImprovedHealth;
+                } else if (assistSuccess == false) {
+                    return -2;
+                }
+            }
+        }
+    }
+}
+    public static HashMap<String,Integer> newFoeHealth(){
+        HashMap<String, Integer> newFoeHealth = new HashMap<String, Integer>();
+
+        return startingHealth;
+
     /**
      * Used to store the member which will be assisted
      */
