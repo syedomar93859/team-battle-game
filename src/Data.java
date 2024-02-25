18a6b060f82505672e6d01f39308d804d3b672b6
syedzayed <syed.omar@ucalgary.ca>
2024-02-24 21:18:13 -0700

    This is for properly understanding commit, push and merge

--- src/Data.java
+++ src/Data.java
@@ -3,14 +3,71 @@
  *  Syed Omar T12
  *  Feb 28, 2024
  */
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
+
+/**SlimeBoss
+10000 health level
+takes turn after all members have had a turn
+
+Special Attack (SLIME CANON)
+deals damage to all of the members (AOE) , takes two turn~ First round announcement~ Second round damage
+800 dmg
+
+net — 02/22/2024 3:09 PM
+Regular Attack
+SlimeTackle
+target one member
+take one turn
+do good amount of damage to one player
+500 dmg
+
+Defense Action
+Reflect Attack
+Only use 2 times during battle
+half of the damage player does will be given back to the player
+Effects player for two rounds
+The player must attack the opponent for two consecutive rounds
+
+arfa — 02/22/2024 3:18 PM
+potion user - 2000 hp
+reg attack - punch. 100 dmg.
+poison (3 times max per battle) - poison status effect lasts for 3 turns to foe. foe takes 150 dmg after foe's turn.
+glass shards (once per 7 turns. once max per battle) - when foe attacks potion user, foe takes 150 dmg
+        run
+assist - raise health by 750 hp for all party members
+
+sword user - 2500 hp
+reg attack - slash, 200 dmg
+wide slash (once per 3 turns) - 150dmg to each foe
+lucky stab (once per 5 turns) - 250 dmg with a 50% chance to double and do 500 dmg
+        run
+assist - raise atk for one party member by 150% for 3 turns.
+
+shield user - 5000 hp
+reg attack - shield bash. 100 dmg
+protect (once per 3 turns) - takes 50% of dmg inflicted to one team member
+party grace (once per 7 turns) - protects all party members
+run
+assist - raise health by 1500 hp for one party member
+*/
 public class Data {
-
+    HashMap<String, Integer> startingHealth = new Hashmap<String, Integer>();
     /**
      * Used to store the member which will be assisted
      */
+    public static boolean exitGame(){
+        return false;
+    }
+    public static boolean storeAttack(String member) {
+        return false;
+    }
+    public static boolean storeRun(){
+        return false;}
+
+
     private static final ArrayList<Object[]> members = new ArrayList<>();
 
     /**
@@ -27,24 +84,16 @@ public class Data {
         System.out.println("Stored member to assist!");
         return true;
     }
+    public static int calculatingEntityHealth(){
 
-    /**
-     * Used to store the attack types
-     */
-    private static final ArrayList<Object[]> attacks = new ArrayList<>();
-
-    /**
-     * Used to check if the attack has been stored
-     *
-     * @param attackType the attack that will be selected
-     * @return true if attack is stored successfully and false if not
-     */
-    public static boolean storeTypeOfAttack(String attackType) {
-        Object[] attack = new Object[2];
-        attack[0] = attackType;
-        attack[1] = false;
-        attacks.add(attack);
-        System.out.println("Stored attack type!");
-        return true;
+    }
+    public static int displayEntityHealth(){
+        for(int entityIndex = 0; entityIndex < entities.length; entityIndex++){
+            if (entityIndex == 0){
+                potioneerHealth =
+            }
+        }
+        return 4;
     }
 }
+
