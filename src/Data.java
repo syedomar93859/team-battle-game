2746bb9303d43534649f636e6970a7ef4a2b97a0
syedzayed <syed.omar@ucalgary.ca>
2024-02-26 20:25:12 -0700

    Added details of all the party members in the beginning of Menu.java. Also added a few extra methods relating to attack enhancement and desperate slash in Data.java

--- src/Data.java
+++ src/Data.java
@@ -3,56 +3,57 @@
  *  Syed Omar T12
  *  Feb 28, 2024
  */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-
 /**SlimeBoss
-10000 health level
-takes turn after all members have had a turn
+ 10000 health level
+ takes turn after all members have had a turn
 
-Special Attack (SLIME CANON)
-deals damage to all of the members (AOE) , takes two turn~ First round announcement~ Second round damage
-800 dmg
+ Special Attack (SLIME CANON)
+ deals damage to all of the members (AOE) , takes two turn~ First round announcement~ Second round damage
+ 800 dmg
 
-net — 02/22/2024 3:09 PM
-Regular Attack
-SlimeTackle
-target one member
-take one turn
-do good amount of damage to one player
-500 dmg
+ net — 02/22/2024 3:09 PM
+ Regular Attack
+ SlimeTackle
+ target one member
+ take one turn
+ do good amount of damage to one player
+ 500 dmg
 
-Defense Action
-Reflect Attack
-Only use 2 times during battle
-half of the damage player does will be given back to the player
-Effects player for two rounds
-The player must attack the opponent for two consecutive rounds
+ Defense Action
+ Reflect Attack
+ Only use 2 times during battle
+ half of the damage player does will be given back to the player
+ Effects player for two rounds
+ The player must attack the opponent for two consecutive rounds
 
-arfa — 02/22/2024 3:18 PM
-potion user - 2000 hp
-reg attack - punch. 100 dmg.
-poison (3 times max per battle) - poison status effect lasts for 3 turns to foe. foe takes 150 dmg after foe's turn.
-glass shards (once per 7 turns. once max per battle) - when foe attacks potion user, foe takes 150 dmg
-        run
-assist - raise health by 750 hp for all party members
+ arfa — 02/22/2024 3:18 PM
+ potion user - 2000 hp
+ reg attack - punch. 100 dmg.
+ poison (3 times max per battle) - poison status effect lasts for 3 turns to foe. foe takes 150 dmg after foe's turn.
+ glass shards (once per 7 turns. once max per battle) - when foe attacks potion user, foe takes 150 dmg
+ run
+ assist - raise health by 750 hp for all party members
 
-sword user - 2500 hp
-reg attack - slash, 200 dmg
-wide slash (once per 3 turns) - 150dmg to each foe
-lucky stab (once per 5 turns) - 250 dmg with a 50% chance to double and do 500 dmg
-        run
-assist - raise atk for one party member by 150% for 3 turns.
+ sword user - 2500 hp
+ reg attack - slash, 200 dmg
+ wide slash (once per 3 turns) - 150dmg to each foe
+ lucky stab (once per 5 turns) - 250 dmg with a 50% chance to double and do 500 dmg
+ run
+ assist - raise atk for one party member by 150% for 3 turns.
 
-shield user - 5000 hp
-reg attack - shield bash. 100 dmg
-protect (once per 3 turns) - takes 50% of dmg inflicted to one team member
-party grace (once per 7 turns) - protects all party members
-run
-assist - raise health by 1500 hp for one party member
-*/
+ shield user - 5000 hp
+ reg attack - shield bash. 100 dmg
+ protect (once per 3 turns) - takes 50% of dmg inflicted to one team member
+ party grace (once per 7 turns) - protects all party members
+ run
+ assist - raise health by 1500 hp for one party member
+ */
+
 public class Data {
     public static HashMap<String,Integer> beginningHealth(){
         HashMap<String, Integer> startingHealth = new HashMap<String, Integer>();
@@ -63,78 +64,227 @@ public class Data {
         return startingHealth;
     }
 
-    public static int actionCheck(String member,int action, int attackAction, String affectedMember) {
-        //attackDetails is a method that I have not created yet that will calculate the whatever the attack is of the member
-        //assistDetermining is a method that determines if an assist is successful as they have a chance to fail
-        //a is a method that
-        if (member == "Potioneer") {
-            //I don't think there should be an if statement checking to see if the option chosen was Exit, as Menu.java
-            //alone can deal with it
-            if (action == 2) {
-                int damage = Data.attackDetails(String member, int attackPotioneerAction);
-                return damage;
+    /**
+     * Used to calculate the damage of a single affect attack
+     *
+     * @param member the member that will be taking action
+     * @param action the action the member will take
+     * @return the int total affect of the action
+     */
+    public static int singleAffect(String member, int action) {
+        int damage = 0;
+        // Determine how much a singular member is healed or singular foe is damaged
+        if (member.equals("Potioneer")) {
+            if (action == 1) { // Regular attack - punch
+                damage = -100; // Damage is represented as negative
+            } else if (action == 2) { // Poison
+                damage = -150;
+            } else if (action == 3) { // Glass shards
+                damage = -150;
             }
-            //I also think Menu.java can deal with Run.
-            else if (action == 4) {
-                boolean assistSuccess = Data.assistDetermining(String member);
-                if (assistSuccess == true) {
-                    return -1;//You should probably come up with something more complicated than -1. Since all the calculating needs to happen in Data.java, there
-                    //could be another method that calulates  the improved health of all the members.
-                } else if (assistSuccess == false) {
-                    return -2;
-                }
-                //I was thinking that Menu.java can use the returned negative number to determine is the assist worked or not.
-            }
-        } else if (member == "Swordsman") {
-            if (action == 2) {
-                int damage = Data.attackDetails(String member, int attackSwordsmanAction);
-                return damage;
-            } else if (action == 4) {
-                boolean assistSuccess = Data.assistDetermining(String member);
-                if (assistSuccess == true) {
-                    return -1;// I think Menu.java should take this -1 and let a loop go on for 3 rounds which would call a different method separate from actionCheck
-                    //in each round to calculate the damage dealt by new attacks.
-                } else if (assistSuccess == false) {
-                    return -2;
+        } else if (member.equals("Swordsman")) {
+            if (action == 1) { // Regular attack - slash
+                damage = -200;
+            } else if (action == 2) { // Lucky stab
+                double chance = Math.random(); // 50% chance to deal double damage
+                if (chance < 0.5) {
+                    damage = -500;
+                } else {
+                    damage = -250;
                 }
             }
-        } else if (member == "Shield User") {
-            if (action == 2) {
-                int damage = Data.attackDetails(String member, int attackShieldUserAction);
-                return damage;
-            } else if (action == 4) {
-                boolean assistSuccess = Data.assistDetermining(String member);
-                if (assistSuccess == true) {
-                    int memberImprovedHealth = Data.assistNewHealth(affectedMember);
-                    //memberImprovedHealth should instead store the improved health of all party members which is not possible with the current actionCheck as it can only return
-                    //int and not hashmaps which would be more suitable.
-                    return memberImprovedHealth;
-                } else if (assistSuccess == false) {
-                    return -2;
-                }
+        } else if (member.equals("Shield User")) {
+            if (action == 1) { // Regular attack - shield bash
+                damage = -100;
             }
         }
+        return damage;
     }
-}
-    public static HashMap<String,Integer> newFoeHealth(){
-        HashMap<String, Integer> newFoeHealth = new HashMap<String, Integer>();
-
-        return startingHealth;
 
     /**
-     * Used to store the member which will be assisted
+     * Used to calculate the damage of an AoE attack
+     *
+     * @param member the member that will be taking action
+     * @param action the action the member will take
+     * @return the int total affect of the action
+     */
+    public static int areaAffect(String member, int action) {
+        int change = 0;
+        if (member.equals("Swordsman")) {
+            if (action == 2) { // Wide slash
+                change = -150;
+            }
+        } else if (member.equals("Shield User")) {
+            if (action == 2) { // Protect
+                change = 0; // No change in health
+            } else if (action == 3) { // Party grace
+                change = 0;
+            }
+        }
+        return change;
+    }
+
+    /**
+     * Used to determine if the action is an AoE or single attack
+     *
+     * @param member the member that will be taking action
+     * @param action the action the member will take
+     * @return the int total affect of the action
+     */
+    public static int attackDetails(String member, int action) {
+        int affect = 0;
+        boolean isAoE = (member.equals("Swordsman") && action == 2) ||
+                (member.equals("Shield User") && (action == 2 || action == 3));
+        if (isAoE) {
+            affect = areaAffect(member, action);
+        } else {
+            affect = singleAffect(member, action);
+        }
+        return affect;
+    }
+
+    /**
+     * Used to determine the improved health
+     *
+     * @param member the member that will be taking action
+     * @return the int total affect of the action
+     */
+    public static int assistNewHealth(String member) {
+        int improvedHealth = 0;
+        if (member.equals("Potioneer")) {
+            improvedHealth = 750; // Potioneer raises health by 750 HP for all party members
+        } else if (member.equals("Shield User")) {
+            improvedHealth = 1500; // Shield User raises health by 1500 HP for one party member
+        }
+        return improvedHealth;
+    }
+
+    /**
+     * Used to determine the improved attack
+     *
+     * @param member the member that will be taking action
+     * @return the int total affect of the action
+     */
+    public static int assistNewAttack(String member) {
+        int improvedAttack = 0;
+        if (member.equals("Swordsman")) {
+            improvedAttack = 150; // Swordsman raises attack for one party member by 150% for 3 turns
+        }
+        return improvedAttack;
+    }
+
+    /**
+     * Used to determine the action taken
+     *
+     * @param member the member that will be taking action
+     * @param action the action the member will take
+     * @param attackAction the int total affect of the action
+     * @param affectedMember the member that will be affected by an assist
+     * @return the int total affect of the action
+     */
+    public static int actionCheck(String member, int action, int attackAction, String affectedMember) {
+        if (member.equals("Potioneer")) {
+            if (action == 2) {
+                int damage = Data.attackDetails(member, attackAction);
+                return damage;
+            } else if (action == 4) {
+                // You should probably come up with something more complicated than -1. Since all the calculating needs to happen in Data.java, there
+                // could be another method that calulates  the improved health of all the members.
+                int improvedHealth = Data.assistNewHealth(affectedMember);
+                return improvedHealth;
+            }
+        } else if (member.equals("Swordsman")) {
+            if (action == 2) {
+                int damage = Data.attackDetails(member, attackAction);
+                return damage;
+            } else if (action == 4) {
+                // I think Menu.java should take this -1 and let a loop go on for 3 rounds which would call a different method separate from actionCheck
+                // in each round to calculate the damage dealt by new attacks.
+                int improvedAttack = Data.assistNewAttack(affectedMember);
+                return improvedAttack;
+            }
+        } else if (member.equals("Shield User")) {
+            if (action == 2) {
+                int damage = Data.attackDetails(member, attackAction);
+                return damage;
+            } else if (action == 4) {
+                // memberImprovedHealth should instead store the improved health of all party members which is not possible with the current actionCheck as it can only return
+                // int and not hashmaps which would be more suitable.
+                int memberImprovedHealth = Data.assistNewHealth(affectedMember);
+                return memberImprovedHealth;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Used to determine the foes' health
+     *
+     * @param foe the foe that will be taking action
+     * @param member the member that will be taking action
+     * @param action the action the member will take
+     * @return the int total affect of the action
+     */
+    public static HashMap<String, Integer> newFoeHealth(String foe, String member, int action) {
+        HashMap<String, Integer> newFoeHealth = new HashMap<String, Integer>();
+
+        // Get the old health of the foe
+        int oldHealth = newFoeHealth.get(foe);
+
+        // Determine if the action is an AoE or not
+        boolean isAoE = (member.equals("Swordsman") && action == 2) ||
+                (member.equals("Shield User") && (action == 2 || action == 3));
+
+        // Calculate the damage based on the action
+        int damage;
+        if (isAoE) {
+            damage = areaAffect(member, action);
+        } else {
+            damage = singleAffect(member, action);
+        }
+
+        // Calculate the new health based on the old health and the damage
+        int newHealth = oldHealth - damage;
+
+        // Update the health of the foe
+        newFoeHealth.put(foe, newHealth);
+
+        return newFoeHealth;
+    }
+
+
+    /**
+     * Used to store the exit
      */
     public static boolean exitGame(){
         return false;
     }
-    public static boolean storeAttack(String member) {
+
+    /**
+     * Used to store the member attack
+     */
+    public static boolean storeAttack(String member, int action) {
+        int damage = singleAffect(member, action);
+        int change = areaAffect(member, action);
+
+        // Store the damage and change values
+        System.out.println("Damage: " + damage);
+        System.out.println("Change: " + change);
+
         return false;
     }
-    public static boolean storeRun(){
-        return false;}
 
 
-    private static final ArrayList<Object[]> members = new ArrayList<>();
+    /**
+     * Used to store the run
+     *
+     * @param member the member that will be taking action
+     * @return the int total affect of the action
+     */
+    public static boolean storeRun(String member) {
+        System.out.println("The character has chosen to run!");
+        return true; // Return true to indicate run was successful.
+    }
 
     /**
      * Used to check if the member has been stored
@@ -142,6 +292,7 @@ public class Data {
      * @param member the member that will be assisted
      * @return true if member is stored successfully and false if not
      */
+    private static final ArrayList<Object[]> members = new ArrayList<>();
     public static boolean storeAssist (String member){
         Object[] id = new Object[2];
         id[0] = member;
@@ -150,16 +301,29 @@ public class Data {
         System.out.println("Stored member to assist!");
         return true;
     }
-    public static int calculatingEntityHealth(){
 
-    }
-    public static int displayEntityHealth(){
-        for(int entityIndex = 0; entityIndex < entities.length; entityIndex++){
-            if (entityIndex == 0){
-                potioneerHealth =
-            }
-        }
-        return 4;
+    public static String displayStat(String member) {
+
+        String stats = "Stats for " + member + ":\n";
+
+        // Add the stats for the member
+        stats += "Health: " + beginningHealth().get(member) + "\n";
+
+        // Add the improved health and attack stats
+        stats += "Improved Health: " + assistNewHealth(member) + "\n";
+        stats += "Improved Attack: " + assistNewAttack(member) + "\n";
+
+        return stats;
+    }public static int desperateSlash(swordsmanHealth) {
+        //This method will determine the damage dealt by the desperate slash of the swordsman using his health.
+        int damage = 160000 / swordsmanHealth;
+        return damage;
+    }public static int attackEnhancment(attackDamage){
+        //This method will take the attack that needs to be enhanced and calculates damage of the enhanced attack.
+        int enhancedDamage = attackDamge * 1.5;
+        return enhancedDamage;
     }
 }
+
+
 
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,38 @@
+/**     Potioneer
+        Current Health: Whatever it is
+
+        Attack Options:
+        Regular Attack - Punch dealing 150 damage.
+        Poison (3 times max per battle) - Causes foe to have poison status effect. Effect lasts for 3 turns to foe. Foe takes 150 damage after its turn.
+        Glass Shards (once per 7 turns. once max per battle) - When foe attacks potion user, foe takes 150 damage.
+
+        Run - Party member leaves battle.
+
+        Assist - Raise health by 750 health point for all party members.
+        -----------------------------------------------------------------------
+        Swordsman
+        Current Health: Whatever it is
+
+        Regular Attack - Slash dealing 200 damage.
+        Desperate Slash (once per 3 turns) - Slash that does more damage, the lower the health of Swordsman.
+        Lucky Stab (once per 5 turns) - Stab that has a 50% change of dealing 250 damage and another 50% chance to deal 500 damage.
+
+        Run - Party member leaves battle.
+
+        Assist - Raise atk for one party member by 150% for 3 turns.
+        -----------------------------------------------------------------------
+        Shield User
+        Current Health: Whatever it is
+
+        Regular Attack - Shield bash which deals 10 dmg.
+        Protect (once per 3 turns) - Takes 50% of damage inflicted to Shield User.
+        Party Grace (once per 7 turns) - Protects all party members from damage.
+
+        Run - Party member leaves battle.
+
+        Assist - Raise health by 1500 hp for one party member.
+        -----------------------------------------------------------------------
+        **/
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Menu {
@@ -13,22 +48,19 @@ public class Menu {
     }
 
     //The method below is the intro message that shows up above the menu.
-
-    private static String menuAndHealth(){
-        String memberName = Data.currentMemberTurn();
-        String uniqueMessage = "\nWhat action do you want " + memberName + " to do? \nMenu Options:\n";
-        return uniqueMessage;
-}
-private static String optMessage = """
+    private static String optMessage = """
             Your team has entered an undiscovered cave entrance and goes inside to take a safe and short rest in the night. All of you have been travelling on a long voyage to seek wealth and power. Unknowingly, they just stepped into the domain of a long gone ancient king called Mammon. Demon King Mammon was a demon that caused calamities wherever it went, by cursing crowds of people and royalty with excessive greed and causing them to go insane. The number of people affected were in the millions and caused families and communities to dissolve. Fortunately, a mysterious hero came from lands far away had happen to come face to face with the beast. His presence lifted the cursed of anyone around him and his weapon, a sword that seemingly bursted out with holy power, cut the beast until no parts of it were left. The hero soon disappeared using magical means that were too advanced for anyone to comprehend, and kingdoms and villages were left to recover for the next century. It has been more than 500 years since the great demon was put to rest, and all the power of the kingdoms and nations had grown even stronger. Old tales remind explorers of the vast riches the demon left behind in its resting grounds that it collected from battlefields it never joined, but won regardless. Whether these tales were myths or not, did not stop hundreds of thousands of adventurers from searching for the ancient beast’s riches all throughout the land.The team of three knew about this story in the back of their mind, but never considered trying to find the grounds as it   seemed impossible for the fact that no-one has discovered such a place for centuries.\s
             They continued deeper into the cave, to eventually face a dead end and settle down. But before they had a chance, a black blob slowly creeped out from a small crevice at the ceiling of the bottom of the cave. The trio took their stances, ready to engage in a fight as the 8 feet tall and 8 feet wide black slime blocked their way out of the cave. Once fully formed, the trio could see jewels, gold coins and other treasures stuck deep in the black slime’s translucent body. Before the trio could consider the riches they may obtain, they moved slightly forward and engaged in a battle
                         
-            BATTLE ENGAGE!\s\n""";
+            BATTLE ENGAGE!\s
+                        
+            Store and access details of the options and actions of the members in the party and also details about foe.
+            \tMenu Options
+            """;
 
     static {
         StringBuilder sb = new StringBuilder();
         sb.append(optMessage);
-        sb.append(menuAndHealth());
         for (int i = 0; i < options.size(); i++) {
             sb.append(String.format("\t%d. %s\n", i, options.get(i)));
         }
@@ -36,7 +68,7 @@ private static String optMessage = """
     }
 
     public static void menuLoop() {
-        System.out.println(optMessage);;
+        System.out.println(optMessage);
         String choice = scanner.nextLine();
         int option = Integer.parseInt(choice);
         while (option != 0) {
@@ -49,6 +81,7 @@ private static String optMessage = """
                 case 1 -> menuEnterChooseAttack();
                 case 2 -> menuEnterChooseRun();
                 case 3 -> menuEnterChooseAssist();
+                case 4 -> menuEnterDisplayStats();
                 default -> System.out.printf("Option %d not recognizable%n", option);
             }
             System.out.println("Press any Enter key to see menu again");
@@ -57,34 +90,52 @@ private static String optMessage = """
             choice = scanner.nextLine();
             option = Integer.parseInt(choice);
         }
-        System.out.println("Thank you for play!");
+        System.out.println("Thank you for playing!");
     }
 
-    private static void menuEnterChooseAssist() {
+    private static void menuEnterDisplayStats()
+    {
         boolean success = false;
         while (!success) {
-            System.out.println("Enter the member to assist:");
+            System.out.println("Which member would you like me to display stats for?");
             String member = scanner.nextLine();
-            success = Data.storeAssist(member);
-        }
-    }
-
-    private static void menuEnterChooseRun() {
-        System.out.println("Enter the member to run:");
-        String member = scanner.nextLine();
-        boolean runSuccess = Data.storeRun();
-        System.out.printf("Run success: %b%n", runSuccess);
-    }
-
-    private static void menuEnterChooseAttack() {
-        boolean success = false;
-        while (!success) {
-            System.out.println("Enter the member and attack type:");
-            String member = scanner.nextLine();
-            String attackType = scanner.nextLine();
-            success = Data.storeAttack(attackType);
+            System.out.println(Data.displayStat(member));
+            System.out.println("Would you like me to display stats for another member? (1- Yes, 2-No)");
+            int answer = scanner.nextInt();
+            if (answer == 2)
+            {
+                success = true;
+            }
         }
     }
 }
+
+
+private static void menuEnterChooseAssist() {
+    boolean success = false;
+    while (!success) {
+        System.out.println("Enter the member to assist:");
+        String member = scanner.nextLine();
+        success = Data.storeAssist(member);
+    }
+}
+
+private static void menuEnterChooseRun() {
+    System.out.println("Enter the member to run:");
+    String member = scanner.nextLine();
+    boolean runSuccess = Data.storeRun();
+    System.out.printf("Run success: %b%n", runSuccess);
+}
+
+private static void menuEnterChooseAttack() {
+    boolean success = false;
+    while (!success) {
+        System.out.println("Enter the member and attack type:");
+        String member = scanner.nextLine();
+        String attackType = scanner.nextLine();
+        success = Data.storeAttack(attackType);
+    }
+}
+
 
 
