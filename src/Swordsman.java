d5372a2c742693915dfb8e1806426a83e17d8ca8
Arfa Raja <arfa.raja@ucalgary.ca>
2024-03-15 03:59:49 -0600

    initialize character files

---
+++ src/Swordsman.java
@@ -0,0 +1,8 @@
+class Swordsman extends Character {
+
+    public Swordsman(String name, int hp, int atk, int def, CharacterType type) {
+
+        super(name, hp, atk, def, CharacterType.SWORDSMAN);
+
+    }
+}
