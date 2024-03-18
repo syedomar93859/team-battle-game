280f0a61bf0a88e14c5b8b25844b3e41f239932a
Arfa Raja <arfa.raja@ucalgary.ca>
2024-03-17 23:12:24 -0600

    add Main.java and Team.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+    /**
+     * Data tracking for group projects in CPSC 233.
+     * Procedural version of program with system.in/out I/O with unit testing and using git for version control.
+     *@authors: Syed Omar, Nethanya Dhaiphule and Arfa Raja
+     *@emails: syed.omar@ucalgary.ca, nethanya.dhaiphule@ucalgary.ca and arfa.raja@ucalgary.ca
+     *@version 21.0.1
+     */
+
+    public class Main {
+        public static void main(String[] args){
+            Menu.menuLoop();
+            System.out.println("Thank you for playing!");
+        }
+    }
+
